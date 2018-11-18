68b30f7a8261f7a3713006847ce85c6462574561
apw50 <apw50@pitt.edu>
2018-11-18 23:00:00 +0000

    again saving
    afsasdfas

--- pi.c
+++ pi.c
@@ -4,44 +4,50 @@
 #include <pthread.h>
 #include <sched.h>
 
-pthread thread1, thread2, thread3;
+pthread_t thread1, thread2, thread3;
 pthread_mutex_t mutex;
 pthread_mutexattr_t mutexattr;
 int policy = SCHED_RR;
 int priority = 6;
 struct sched_param param;
 
-void callFunc()
-{
-	//do some work here.
+void callFunc() {
+	printf("in callFunc");
 }
 
-void funtion1() {
+void func1() {
 	pthread_mutex_lock(&mutex);
-	
-	//function 1 for thread 1.
-	//do some work here
+	printf("entering func1");
+	printf("exiting func1");
 	pthread_mutex_unlock(&mutex);
 }
 
-void function2()
-{
-	//function 2 for thread 2.
-	//do some work here and callFunc()
-	
-	callFunc()
+void func2() {
+	pthread_mutex_lock(&mutex);
+	printf("entering func2");
+	callFunc();
+	printf("exiting func2");
+	pthread_mutex_unlock(&mutex);
 }
 
-void function3()
-{
-	//function 3 for thread 3.
-	//do some work here
+void func3() {
+	pthread_mutex_lock(&mutex);
+	printf("entering func3");
+	printf("exiting func3");
+	pthread_mutex_unlock(&mutex);
 }
 
-void main()
-{
+int main() {
 	//create mutex and initialize it.
 	//create thread 1, thread 2 and thread 3
 	//set different priority for each thread
 	//set handler for thread 1, thread 2 and thread 3 respectively. For instance, function 1 for thread 1, function 2 for thread 2 and function 3 for thread 3
+	
+	pthread_mutexattr_setprotocol(&mutexattr, PTHREAD_PRIO_INHERIT);
+	pthread_mutex_init(&mutex, &mutexattr);
+	priority = sched_get_priority_max(policy);
+	param.sched_priority = priority;
+	pthread_setschedparam(thread1, policy,&param);
+	pthread_join(thread1,NULL);
+	return 0;
 }
